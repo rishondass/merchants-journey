3dd859c4e83b00178fb093bc1cef67f3b6d922d1
Rishon Dass <rishondass@gmail.com>
2024-05-29 18:44:45 -0500

    clean up

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,41 +1,9 @@
-import { io, type Socket } from "socket.io-client";
+import {io} from "socket.io-client";
 
-const URL = "http://10.0.0.96:4000";
-
-const socket = io(URL,{
+const socket = io(process.env.NEXT_PUBLIC_SERVER_URL??"",{
 	autoConnect: false,
 	transports:["websocket","polling"]
 })
 
 export default socket;
-
-// interface ISocketConn{
-// 	socket: Socket;
-// 	connect: ()=>void;
-// 	getSocket: ()=> Socket;
-// 	printSocket:()=> void;
-// }
-
-// class SocketConn implements ISocketConn {
-// 	public socket: Socket;
-
-// 	constructor(){
-// 		this.socket = io(URL,{autoConnect:false});
-// 	}
-
-// 	printSocket=()=>{
-// 		console.log(this.socket.connected);
-// 	}
-
-// 	connect = ()=>{
-// 		this.socket.connect();
-// 	}
-
-// 	getSocket = ():Socket=>{
-// 		return this.socket;
-// 	}
-// }
-
-// export default SocketConn;
-
 
